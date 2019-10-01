e8c12152fd24a7459f46543c361ea5ab79271a05
pmajtas <majtas.pawel1@gmail.com>
2019-10-01 21:50:46 +0200

    cwiczenia 1, 2

---
+++ AVR_Meter/AssemblerApplication1/AssemblerApplication1/main.asm
@@ -0,0 +1,24 @@
+;
+; AssemblerApplication1.asm
+;
+; Created: 2019-10-01 20:15:00
+; Author : Moj
+;
+
+
+; Replace with your application code
+
+;cw2
+;LDI R16, 3
+;LDI R17, 1
+;MOV R0, R16
+;SUB R0, R17 
+;SUB R0, R17
+;SUB R0, R17
+
+; æw1
+;LDI R20, 14
+;LDI R21, 17
+;MOV R0, R20
+;MOV R1, R21
+;ADD R0, R1
